ceb96ece9cf1c8f3f1a7a81b5448e2b7cfd39f20
Andries <nieu0082@hz.nl>
2017-11-29 14:40:30 +0100

    implementatie dice

---
+++ BlueC_Dice/BlueC_Dice/Dobbelsteen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueC_Dice
+{
+    public class Dobbelsteen
+    {
+        private int Waarde;
+        private int AantalVlakken;
+
+        public Dobbelsteen()
+        {
+            this.Waarde = 2;
+            AantalVlakken = 6;
+        }
+
+        public void Roll()
+        {
+            this.Waarde += 1;
+        }
+
+        public int GetValue()
+        {
+            return (Waarde % AantalVlakken) + 1;
+        }
+    }
+}
