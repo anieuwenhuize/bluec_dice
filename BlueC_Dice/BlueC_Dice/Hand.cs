c151a084ce0096d0a1e5becb5b3789c682f06a0c
Andries <nieu0082@hz.nl>
2017-12-06 13:04:19 +0100

    Uitwerkingen er in

--- BlueC_Dice/BlueC_Dice/Hand.cs
+++ BlueC_Dice/BlueC_Dice/Hand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,46 +7,64 @@ namespace BlueC_Dice
 {
     public class Hand
     {
-        private Dobbelsteen Dice1;
-        private Dobbelsteen Dice2;
-        private Dobbelsteen Dice3;
-        private Dobbelsteen Dice4;
-        private Dobbelsteen Dice5;
-        private Dobbelsteen Dice6;
+        private Dobbelsteen[] Dobbelstenen;
 
         public Hand()
         {
-            this.Dice1 = new Dobbelsteen();
-            this.Dice2 = new Dobbelsteen();
-            this.Dice3 = new Dobbelsteen();
-            this.Dice4 = new Dobbelsteen();
-            this.Dice5 = new Dobbelsteen();
-            this.Dice6 = new Dobbelsteen();
+            // Manier 1: collection initializer
+            this.Dobbelstenen = new Dobbelsteen[]
+            {
+                new Dobbelsteen(),
+                new Dobbelsteen(),
+                new Dobbelsteen(),
+                new Dobbelsteen(),
+                new Dobbelsteen(),
+                new Dobbelsteen()
+            };
+
+            // Manier 2: for-statement
+            int aantal = 6;
+            this.Dobbelstenen = new Dobbelsteen[aantal];
+            for(int i=0; i< aantal; i++)
+            {
+                this.Dobbelstenen[i] = new Dobbelsteen();
+            }
         }
 
         public void Werp()
         {
-            this.Dice1.Roll();
-            this.Dice2.Roll();
-            this.Dice3.Roll();
-            this.Dice4.Roll();
-            this.Dice5.Roll();
-            this.Dice6.Roll();
+            foreach(Dobbelsteen dobbelsteen 
+                in this.Dobbelstenen)
+            {
+                dobbelsteen.Roll();
+            }
         }
 
         public int GetValue()
         {
-            // Geeft de waarde van alle dobbelstenen terug
-            int waarde = this.Dice1.GetValue()
-                + this.Dice2.GetValue()
-                + this.Dice3.GetValue()
-                + this.Dice4.GetValue()
-                + this.Dice5.GetValue()
-                + this.Dice6.GetValue();
+            // Manier 1: imperatief: HOE
+            int totaalwaarde = 0;
 
-            // de rest mag je zelf doen.
+            for(int i=0;i<this.Dobbelstenen.Length; i++)
+            {
+                Dobbelsteen huidigedobbelsteen = this.Dobbelstenen[i];
 
-            return waarde;
+                totaalwaarde = totaalwaarde + huidigedobbelsteen.GetValue();
+            }
+
+            return totaalwaarde;
+            
+            // Manier 2: declaratief: WAT
+            int result =
+                this.Dobbelstenen
+
+                    // Geef van iedere dobbelsteen de waarde terug
+                    .Select(dobbelsteen => dobbelsteen.GetValue())
+
+                    // Tel al deze waardes op
+                    .Count();
+
+            return result;
         }
     }
 }
